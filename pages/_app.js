bd25cc7d1feaa9407c4507f24d423e0a3a21eed7
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-08-18 00:59:49 +0500

    layout developed and set in the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
+import "../styles/fonts.css";
+import Layout from "./layout";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
+
+export default MyApp;
