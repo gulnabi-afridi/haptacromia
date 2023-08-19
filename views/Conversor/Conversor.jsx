fbf8d3aae3759b6b11b1b71c5a3263f826f083d7
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-08-19 13:21:02 +0500

    conversor-page layout set

---
+++ views/Conversor/Conversor.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import { Box } from "@mui/material";
+// import Header from "../../components/Conversor/Header";
+// import Contrast from "../../components/Conversor/Contrast/Contrast";
+// import Formulas from "../../components/Conversor/Formulas/Formulas";
+// import CustomeColorPicker from "../../components/Conversor/Formulas/ColorPicker";
+// import MiddleMain from "../../components/Conversor/Middle/MiddleMain";
+
+function Conversor() {
+  return (
+    <>
+      <Box
+        sx={{
+          flexGrow: 1,
+          width: "100%",
+          background: "#D8D8D8",
+        }}
+      >
+        {/* Conversor Header */}
+
+        {/* <Header /> */}
+
+        <Grid container spacing={{ xs: 1, xmd: 2 }}>
+          {/* first column */}
+          <Grid
+            item
+            sx={{
+              minHeight: "100vh",
+            }}
+            xs={3.8}
+          >
+            {/* <Formulas /> */}
+            {/* <CustomeColorPicker/> */}
+          </Grid>
+
+          {/* secound column */}
+          <Grid item sx={{ minHeight: "100vh" }} xs={5.5}>
+            {/* <MiddleMain/> */}
+          </Grid>
+
+          {/* third column */}
+          <Grid item sx={{ minHeight: "100vh" }} xs={2.5}>
+            {/* <Contrast /> */}
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
+}
+
+export default Conversor;
