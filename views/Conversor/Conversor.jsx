542c9cd59baec378cf88754de7bb646b1979b640
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-08-23 11:14:31 +0500

    contrast section added

--- views/Conversor/Conversor.jsx
+++ views/Conversor/Conversor.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import { Box } from "@mui/material";
 import Header from "@/components/Conversor/Header";
-// import Contrast from "../../components/Conversor/Contrast/Contrast";
+import Contrast from "@/components/Contrast/Contrast";
 import Formulas from "../../components/Conversor/Formulas/Formulas";
 import CustomeColorPicker from "../../components/Conversor/Formulas/ColorPicker";
 import MiddleMain from "@/components/Middle/MiddleMain";
@@ -41,7 +41,7 @@ function Conversor() {
 
           {/* third column */}
           <Grid item sx={{ minHeight: "100vh" }} xs={2.5}>
-            {/* <Contrast /> */}
+            <Contrast />
           </Grid>
         </Grid>
       </Box>
